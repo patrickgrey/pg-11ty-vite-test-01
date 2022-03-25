74da7f7fc9a1ea331bf3159cbe9214a2cd824d34
patrickgrey <websemantic@yahoo.co.uk>
2022-03-25 10:17:23 +0000

    Simplified example further

--- .eleventy.js
+++ .eleventy.js
@@ -32,7 +32,7 @@ module.exports = function (eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    // pathPrefix: "/",
     // -----------------------------------------------------------------
 
     // These are all optional (defaults are shown):
